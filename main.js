25eae48182b8c6fb76da20ef23ff4b909352f060
enochtagoe <enochtagoe@pursuit.org>
2023-11-16 15:09:04 -0500

    push updates

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 // console.log("connected")
+const form = document.querySelector("form")
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+// console.log(event.target.submit.value)
 
-let wordToSearch = "house"
-fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToSearch}`).then(response => response.json()).then(data=> console.log(data));
-event.preventDefault();
-console.log("Submit:", event.target.submit.value);
+    const wordToSearch = event.target.submit.value
+fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToSearch}`).then(response => response.json()).then(data=> {console.log(data)
+data[0].meanings[0].definitions.forEach(item => {
+    console.log(item.definition);
+    const definition = document.createElement("div")
+    definition.textContent = item.definition
+    form.after(definition);
+}
+)
+});
+// reset the form
+form.reset()
+
+
+
+} )
+
+
+
+
+ 
+// console.log("Submit:", event.target.submit.value);
 // console.log("Submit:", event.target.submit.value);
 
 
